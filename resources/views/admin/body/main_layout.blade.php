4b2e1ec13aca31eaa1b1eb738b21ef50d9efad2a
almamun2s <mamun2social@gmail.com>
2024-05-17 20:29:44 +0600

    dashboard segmented

---
+++ resources/views/admin/body/main_layout.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>@yield('title') | News Website </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+
+    <!-- Plugins css -->
+    <link href="{{ asset('backend/assets/libs/flatpickr/flatpickr.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backend/assets/libs/selectize/css/selectize.bootstrap3.css') }}" rel="stylesheet"
+        type="text/css" />
+
+    <!-- Bootstrap css -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App css -->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
+    <!-- icons -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Head js -->
+    <script src="{{ asset('backend/assets/js/head.js') }}"></script>
+
+</head>
+
+<!-- body start -->
+
+<body data-layout-mode="default" data-theme="light" data-topbar-color="dark" data-menu-position="fixed"
+    data-leftbar-color="light" data-leftbar-size='default' data-sidebar-user='false'>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+
+
+        @include('admin.body.topbar')
+
+        @include('admin.body.left_sidebar')
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+
+                @yield('content')
+
+            </div> <!-- content -->
+
+            <!-- Footer Start -->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> &copy; resorved by <a href="">Abdullah Almamun</a>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="text-md-end footer-links d-none d-sm-block">
+                                <a href="javascript:void(0);">About Us</a>
+                                <a href="javascript:void(0);">Help</a>
+                                <a href="javascript:void(0);">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- end Footer -->
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+
+    </div>
+    <!-- END wrapper -->
+
+    @include('admin.body.right_sidebar')
+
+    <!-- Vendor js -->
+    <script src="{{ asset('backend/assets/js/vendor.min.js') }}"></script>
+
+    <!-- Plugins js-->
+    <script src="{{ asset('backend/assets/libs/flatpickr/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+    <script src="{{ asset('backend/assets/libs/selectize/js/standalone/selectize.min.js') }}"></script>
+
+    <!-- Dashboar 1 init js-->
+    <script src="{{ asset('backend/assets/js/pages/dashboard-1.init.js') }}"></script>
+
+    <!-- App js-->
+    <script src="{{ asset('backend/assets/js/app.min.js') }}"></script>
+
+</body>
+
+</html>
